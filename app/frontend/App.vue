ea9e69db9890864867fc60c936922c56b90a412d
Tropometrie <loucifishaq1@gmail.com>
2025-03-02 11:37:56 -0500

    these files include the entry point 'App.vue', as well as other core files needed to run the app on the browser.

---
+++ app/frontend/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+// Import the components here :
+import Home from './components/Home.vue';
+import dmChatBox from './components/dmChatBox.vue';
+
+export default {
+  components: {
+    Home,
+    dmChatBox,
+  },
+};
+</script>
+
+<style>
+/* Add here the CSS code for the global styling (global) */
+</style>
